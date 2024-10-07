af5f5e5974c077636665d4e70752c4a85164368c
Nguyễn Anh Hào <anhhao012004@gmail.com>
2024-10-07 23:38:05 +0700

    feat(ri-14): move components to global

---
+++ src/frontend/src/components/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Container, Box, Button } from '@mui/material';
+import NavItem from './NavItem';
+
+const Header: React.FC = () => {
+    return (
+        <AppBar position="static" style={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
+            <Container maxWidth="lg">
+                <Toolbar disableGutters>
+                    <Box display="flex" alignItems="center" flexGrow={1} gap="8px">
+                        <Box display="flex" alignItems="center" gap="16px" style={{ padding: '0px  12px 0px 0px' }}>
+                            <img src="/Logo.png" alt="Logo" style={{ width: '40px', height: '40px' }} />
+                            <Typography variant="h6" component="div" style={{ color: 'black' }}>
+                                RiCourse
+                            </Typography>
+                        </Box>
+                        <NavItem text="Khóa học" />
+                        <NavItem text="Kỳ thi" />
+                        <NavItem text="Diễn đàn" />
+                    </Box>
+                    <Box display="flex" alignItems="center" gap="16px">
+                        <Button color="inherit" style={{ color: 'black' }}>Đăng nhập</Button>
+                        <Button color="inherit" style={{ color: 'white', backgroundColor: 'black' }}>Đăng ký</Button>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+
+export default Header;
