2460f1e85913c85b70bb1be12221cf2b3d278718
Nguyễn Anh Hào <anhhao012004@gmail.com>
2024-10-07 23:40:39 +0700

    feat(ri-14): modify app/layout.tsx, app/page.tsx

--- src/frontend/src/app/page.tsx
+++ src/frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Container, Box, Typography, Grid, TextField, Button } from "@mui/material";
-import Header from "./components/Header";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
