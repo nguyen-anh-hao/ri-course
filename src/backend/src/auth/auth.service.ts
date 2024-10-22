c77298a1f2e5306d44cfe4687050613bc950405f
theworstkhoi <kylekhoitv@gmail.com>
2024-10-22 14:42:36 +0700

    feat(ri-17): added bcrypt for signin and signup

--- src/backend/src/auth/auth.service.ts
+++ src/backend/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { UsersService } from "src/users/users.service";
 import { SignInDto, SignUpDto } from "./dtos";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
@@ -12,10 +13,12 @@ export class AuthService {
 
     async validateUser(username: string, password: string) {
         const user = await this.usersService.findOne(username);
+        if (user == null)
+            return null;
 
-        if (user && user.password === password) { // add some decryption here
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch)
             return user;
-        }
 
         return null;
     }
@@ -37,6 +40,11 @@ export class AuthService {
         if (isTaken === true)
             throw new BadRequestException("Username is already taken");
 
-        return await this.usersService.createOne(user);
+        const hashedPassword = await bcrypt.hash(user.password, 11);
+
+        return await this.usersService.createOne({
+            ...user,
+            password: hashedPassword
+        });
     }
 }
