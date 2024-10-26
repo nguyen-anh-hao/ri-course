02ed5f68c01e7647e1f2c452aead1c18e0c18d8d
Nguyễn Anh Hào <anhhao012004@gmail.com>
2024-10-26 16:51:31 +0700

    feat(ri-36, ri-47, ri-48): add all-courses page, my-courses page

---
+++ src/frontend/src/app/all-courses/page.tsx
@@ -0,0 +1,19 @@
+import CourseCard from "@/components/ui/CourseCard";
+import { Box, Container, Grid } from "@mui/material";
+
+export default function AllCourses() {
+    return (
+        <Container component="main" maxWidth="lg">
+            <Grid container spacing={2} justifyContent="left">
+                {Array.from({ length: 14 }).map((_, index) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} sx={{ minWidth: 260 }} key={index}>
+                        <CourseCard
+                            name={`Lập trình React ${index + 1}`}
+                            mentor="Nguyễn Văn A"
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    );
+}
