78ec9cdb898b3437d9a3d5215fad162ab237e015
Nguyễn Anh Hào <anhhao012004@gmail.com>
2024-12-28 05:40:49 +0700

    test: update signup tests to use specific input IDs and improve password confirmation checks

--- src/frontend/cypress/e2e/signup.cy.js
+++ src/frontend/cypress/e2e/signup.cy.js
@@ -7,20 +7,24 @@ describe('Sign up functionality', () => {
         const password = user;
 
         cy.visit(`${cypress_base_url}/auth/sign-up`);
-        cy.get('input[type="text"]').type(user);
-        cy.get('input[type="text"]').should('have.value', user);
-        cy.get('input[type="password"]').type(password);
-        cy.get('input[type="password"]').should('have.value', password);
+        cy.get('input[id=":r0:"]').type(user);  // Tên đăng nhập
+        cy.get('input[id=":r0:"]').should('have.value', user);
+        cy.get('input[id=":r1:"]').type(password);  // Mật khẩu
+        cy.get('input[id=":r1:"]').should('have.value', password);
+        cy.get('input[id=":r2:"]').type(password);  // Xác nhận mật khẩu
+        cy.get('input[id=":r2:"]').should('have.value', password);
         cy.get('button[type="submit"]').contains('Đăng ký').click();
         cy.url().should('eq', `${cypress_base_url}/auth/sign-in`);
     });
 
     it('should display error message with invalid credentials', () => {
         cy.visit(`${cypress_base_url}/auth/sign-up`);
-        cy.get('input[type="text"]').type('user1');
-        cy.get('input[type="text"]').should('have.value', 'user1');
-        cy.get('input[type="password"]').type('user1');
-        cy.get('input[type="password"]').should('have.value', 'user1');
+        cy.get('input[id=":r0:"]').type('user1');  // Tên đăng nhập
+        cy.get('input[id=":r0:"]').should('have.value', 'user1');
+        cy.get('input[id=":r1:"]').type('user1');  // Mật khẩu
+        cy.get('input[id=":r1:"]').should('have.value', 'user1');
+        cy.get('input[id=":r2:"]').type('user1');  // Xác nhận mật khẩu
+        cy.get('input[id=":r2:"]').should('have.value', 'user1');
         cy.get('button[type="submit"]').contains('Đăng ký').click();
         cy.get('.MuiAlert-message')
             .should('be.visible')
@@ -37,12 +41,12 @@ describe('Sign up functionality', () => {
 
     it('should display error message with mismatched passwords', () => {
         cy.visit(`${cypress_base_url}/auth/sign-up`);
-        cy.get('input[type="text"]').type('user1');
-        cy.get('input[type="text"]').should('have.value', 'user1');
-        cy.get('input[type="password"]').type('user1');
-        cy.get('input[type="password"]').should('have.value', 'user1');
-        cy.get('input[type="password"]').eq(1).type('user2');
-        cy.get('input[type="password"]').eq(1).should('have.value', 'user2');
+        cy.get('input[id=":r0:"]').type('user1');  // Tên đăng nhập
+        cy.get('input[id=":r0:"]').should('have.value', 'user1');
+        cy.get('input[id=":r1:"]').type('user1');  // Mật khẩu
+        cy.get('input[id=":r1:"]').should('have.value', 'user1');
+        cy.get('input[id=":r2:"]').type('user2');  // Xác nhận mật khẩu
+        cy.get('input[id=":r2:"]').should('have.value', 'user2');
         cy.get('button[type="submit"]').contains('Đăng ký').click();
         cy.get('.MuiAlert-message')
             .should('be.visible')
