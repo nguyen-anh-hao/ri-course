78fa32364250d415aa35057c9bccc92e852c860a
Nguyễn Anh Hào <anhhao012004@gmail.com>
2025-01-21 19:29:36 +0700

    test: add Cypress test for backend connection

---
+++ src/frontend/cypress/e2e/connectToBackend.cy.js
@@ -0,0 +1,8 @@
+descibe('Connect to backend', () => {
+    it('Connect to backend', () => {
+        cy.request('GET', 'http://localhost:3123/api/')
+            .then((response) => {
+                expect(response.status).to.eq(200);
+            });
+    });
+});
