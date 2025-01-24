e1410bdb3bf948db8c05119f5df506286be55bcc
Nguyễn Anh Hào <anhhao012004@gmail.com>
2025-01-24 12:04:56 +0700

    Update Node.js version in CI and fix backend API request URL in Cypress test

--- src/frontend/cypress/e2e/connectToBackend.cy.js
+++ src/frontend/cypress/e2e/connectToBackend.cy.js
@@ -1,6 +1,6 @@
 describe('Connect to backend', () => {
     it('Connect to backend', () => {
-        cy.request('GET', 'http://localhost:3123/api/').then((response) => {
+        cy.request('GET', 'http://localhost:3000/api/api/').then((response) => {
             expect(response.status).to.eq(200);
         });
     });
